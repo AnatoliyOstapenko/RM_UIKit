cfdecb7e7fa4048e6671b76d6af57f5e5775660b
Anatoliy Ostapenko <anatoliy.ostapenko@gmail.com>
2025-02-04 17:41:50 +0200

    Initial Commit

---
+++ RM_UIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RM_UIKit
+//
+//  Created by Anatoliy Ostapenko on 04.02.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
